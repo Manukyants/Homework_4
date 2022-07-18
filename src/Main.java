bbb6d00dff6d31e0f9c9e5d77e909b69198b58cd
Манукянц Бабкен <b.manukyants@gmail.com>
2022-07-18 18:05:03 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,58 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задание №1
+        int i = 10;
+        while(i > 0){
+            System.out.print(i + " ");
+            i--;
+        }
+        System.out.println("\n");
+        for (i = 1; i < 11; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        // Задание №2
+        int friday = 5;
+        for (friday = 5; friday < 31; friday = friday + 7){
+            System.out.println("Сегодня пятница " + friday + "-е число. Необходимо подготовить отчет");
+        }
+
+
+        // Задание №3
+        int y = 2022;
+        int f = y + 100;
+        int k = y - 200;
+        for (y = 0; y < f; y = y + 79 ){
+            if (y<f && y>k) {
+                System.out.println(y);
+            }
+        }
+
+     
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+
+
+
+        }
+        
     }
-}
